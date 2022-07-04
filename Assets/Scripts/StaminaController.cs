1267379df0be5a1a4cdc25bf3a9b45f06f165155
aaqqssaall <aqsal.haris@gmail.com>
2022-07-04 08:32:51 +0700

    Add Stamina Bar UI

--- Assets/Scripts/StaminaController.cs
+++ Assets/Scripts/StaminaController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class StaminaController : MonoBehaviour
-{ 
+{
+    public Slider staminaBar;
+
     public float Stamina = 100.0f;
     public float MaxStamina = 100.0f;
     //---------------------------------------------------------
@@ -15,18 +17,28 @@ public class StaminaController : MonoBehaviour
     private const float StaminaTimeToRegen = 2.0f;
     //---------------------------------------------------------
 
+    private void Start()
+    {
+        staminaBar.maxValue = MaxStamina;
+        staminaBar.value = MaxStamina;
+    }
+
     private void Update()
     {
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         if (isRunning)
         {
             Stamina = Mathf.Clamp(Stamina - (StaminaDecreasePerFrame * Time.deltaTime), 0.0f, MaxStamina);
+            staminaBar.value = Stamina;
             StaminaRegenTimer = 0.0f;
         }
         else if (Stamina < MaxStamina)
         {
             if (StaminaRegenTimer >= StaminaTimeToRegen)
+            {
                 Stamina = Mathf.Clamp(Stamina + (StaminaIncreasePerFrame * Time.deltaTime), 0.0f, MaxStamina);
+                staminaBar.value = Stamina;
+            }
             else
                 StaminaRegenTimer += Time.deltaTime;
         }
