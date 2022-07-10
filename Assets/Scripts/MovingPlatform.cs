0ebae0953045cba8708bec7743f44be4ca32829a
atzahir <63177302+atzahir@users.noreply.github.com>
2022-07-10 20:31:35 +0700

    Level 1 update

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    public int startingPoint;
+    public Transform[] points;
+    
+    private int i; //index of array
+    void Start()
+    {
+        //setting the position of the platform to the position of one of the points using index "startingPoint"
+        transform.position = points[startingPoint].position; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // checking the distance of the platform and the point
+        if(Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++; //increase the index
+            if(i == points.Length) // check if the platform was on the last point after the index increase
+            {
+                i = 0;
+            }
+        }
+        // moving the platform to the point position with the index "i"
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.position.y > transform.position.y)
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
